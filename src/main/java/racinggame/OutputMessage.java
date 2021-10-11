1f39d1283f0c4db08d490172357a9acb5309fcd2
naseu <naseunghoo@naver.com>
2021-10-11 11:48:46 +0900

    feat(OutputMessage): 화면 메시지 출력 기능 추가
    - 한 바퀴마다 각 자동차 현재 위치 표시 추가
    - 최종우승자 메시지 출력 추가

---
+++ src/main/java/racinggame/OutputMessage.java
@@ -0,0 +1,30 @@
+package racinggame;
+
+import java.util.List;
+import java.util.Map;
+
+public class OutputMessage {
+    static private final StringBuilder sb = new StringBuilder();
+
+    public static void print(Map<Car, List<CarStatus>> carMaps) {
+        for (Car car : carMaps.keySet()) {
+            sb.append(car.getName());
+            sb.append(" : ");
+            sb.append(getCarPositionHistory(carMaps, car));
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static String getCarPositionHistory(Map<Car, List<CarStatus>> carMaps, Car car) {
+        StringBuilder sb = new StringBuilder();
+        for (CarStatus carStatus : carMaps.get(car)) {
+            sb.append(carStatus.marker());
+        }
+        return sb.toString();
+    }
+
+    public static void print(String str) {
+        System.out.printf("최종우승자는 %s 입니다.", str);
+    }
+}
