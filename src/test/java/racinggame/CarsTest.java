8d3ae10d0f600bc507d107593a81eb7a488f063e
naseu <naseunghoo@naver.com>
2021-10-12 09:41:53 +0900

    test(CarTest): 우승자 확인 테스트 수정
    - 우승자 확인에서 최종 우승자 메시지 검증 추가

--- src/test/java/racinggame/CarsTest.java
+++ src/test/java/racinggame/CarsTest.java
@@ -1,6 +1,8 @@
 package racinggame;
 
+import nextstep.test.NSTest;
 import nextstep.utils.Randoms;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -11,7 +13,12 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CarsTest {
+public class CarsTest extends NSTest {
+
+    @BeforeEach
+    void beforeEach() {
+        setUp();
+    }
 
     @Test
     void 자동차_여러대를_생성() {
@@ -51,5 +58,10 @@ public class CarsTest {
             );
         }
         cars.winner();
+        verify("최종 우승자는 테슬라 입니다.");
+    }
+
+    @Override
+    protected void runMain() {
     }
 }
