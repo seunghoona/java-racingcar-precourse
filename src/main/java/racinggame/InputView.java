aa31e7a0ced6f3ea1a2072d417fc52461ad18a8e
naseu <naseunghoo@naver.com>
2021-10-12 10:17:35 +0900

    fix(InputView): validation 실패시 메소드 재호출 추가
    - catch exception에서 메소드 호출 추가
    - 테스트 케이스 수정 및 정규식 변경

--- src/main/java/racinggame/InputView.java
+++ src/main/java/racinggame/InputView.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 
 public class InputView {
 
-    private static final String COMMA = "(\\w+,\\w+)|\\w+";
-    private static final String LAST_NOT_COMMA = "[\\w\\d]+,$";
+    private static final String COMMA = "([\\w\\d].+)+([^\\w,]).+";
+    private static final String LAST_NOT_COMMA = "(\\w+([\\d\\w]),)*";
     private static final String ONLY_NUMBER = "[0-9]+";
 
     Circuit circuit;
@@ -15,22 +15,23 @@ public class InputView {
     public InputView() {
         circuit = new Circuit();
     }
+
     /*
         사용자 자동차 입력시 입력 포맷 확인  pobi,bmw,tico
         허용 : pobi,bmw,tico
         exception : 콤마로 구분해서 입력하지 않는경우 한건인경우는 exception 발생하지 않음
      */
-    static void validationInputCar(String inputCar) {
+    void validationInputCar(String inputCar) {
         if (Pattern.matches(LAST_NOT_COMMA, inputCar)) {
             throw new IllegalArgumentException("[ERROR] ,를 마지막에 입력하면 안됩니다.");
         }
 
-        if (!Pattern.matches(COMMA, inputCar)) {
+        if (Pattern.matches(COMMA, inputCar)) {
             throw new IllegalArgumentException("[ERROR] 자동차는 ,구분해서 입력해야합니다.");
         }
     }
 
-    static void validationInputLaps(String laps) {
+    void validationInputLaps(String laps) {
         if (!Pattern.matches(ONLY_NUMBER, laps)) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력해야합니다.");
         }
@@ -44,6 +45,7 @@ public class InputView {
             circuit.createCar(inputCar);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            inputCar();
         }
     }
 
@@ -55,6 +57,7 @@ public class InputView {
             circuit.start(Integer.parseInt(laps));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            laps();
         }
     }
 
--- src/test/java/racinggame/InputViewTest.java
+++ src/test/java/racinggame/InputViewTest.java
@@ -3,38 +3,47 @@ package racinggame;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
+    InputView inputView;
 
-    @Test
-    @DisplayName("마지막 콤마 추가시 Excpetion")
-    void validationLastComma() {
-        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
-            InputView.validationInputCar("123,");
-        })
-        .withMessageStartingWith("[ERROR]")
-        .withMessageContaining(",를 마지막에 입력하면 안됩니다.");
+    public InputViewTest() {
+        this.inputView = new InputView();
     }
 
-    @Test
+    @ParameterizedTest
+    @DisplayName("마지막 콤마 추가시 Excpetion")
+    @ValueSource(strings = {"123,", "1234,123,", "bmw,audi,k5,kia,"})
+    void validationLastComma(String carName) {
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
+                    inputView.validationInputCar(carName);
+                })
+                .withMessageStartingWith("[ERROR]")
+                .withMessageContaining(",를 마지막에 입력하면 안됩니다.");
+    }
+
+    @ParameterizedTest
     @DisplayName("자동차 입력시 콤마에 맞게 입력되는 확인")
+    @ValueSource(strings = {"123,", "1234,123,", "bmw,audi,k5,kia,"})
     void validationCommaCheck() {
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
-            InputView.validationInputCar("123/123");
-        })
-        .withMessageStartingWith("[ERROR]")
-        .withMessageContaining("자동차는 ,구분해서 입력해야합니다.");
+                    inputView.validationInputCar("123/123");
+                })
+                .withMessageStartingWith("[ERROR]")
+                .withMessageContaining("자동차는 ,구분해서 입력해야합니다.");
     }
 
     @Test
     @DisplayName("정수 외 입력시 오류")
     void validationOnlyNumber() {
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
-            InputView.validationInputLaps("asdf");
-        })
-        .withMessageStartingWith("[ERROR]")
-        .withMessageContaining("숫자만 입력해야합니다.");
+                    inputView.validationInputLaps("asdf");
+                })
+                .withMessageStartingWith("[ERROR]")
+                .withMessageContaining("숫자만 입력해야합니다.");
     }
 
 
